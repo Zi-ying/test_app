ce51071568ec5a7c879daa5a4cbe883a89f981b3
Sophie Bel <sophie.belmarrocco@gmail.com>
2022-03-14 16:55:04 +0100

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'about', to: 'pages#about'
+  # get resquest to about page
+  # to: is where the reauest is sent to, here the controller (name and action) 
+  # then you need to put the method into the pages controller.
+  # then we need to create the file in view > pages
 end
