d6f9f16411c68af1fe25210aa007757093ac7b48
Sophie Bel <sophie.belmarrocco@gmail.com>
2022-03-28 17:56:15 +0200

    add untracked files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
   get 'about', to: 'pages#about'
   # get resquest to about page
-  # to: is where the reauest is sent to, here the controller (name and action) 
+  # to: is where the request is sent to, here the controller (name and action) 
   # then you need to put the method into the pages controller.
   # then we need to create the file in view > pages
 end
